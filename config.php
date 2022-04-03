f2c775b9906f61a78d32925d49d859120e56be8b
niniengC <ninieng.choirunnisa@gmail.com>
2022-04-03 23:34:45 +0700

    File config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+#koneksi ke database
+$dbHost ="localhost";
+$dbUser = "root";
+$dbPassword = "";
+$dbName= "ninieng";
+
+$koneksi = mysqli_connect($dbHost, $dbUser, $dbPassword, $dbName);
+
+?>
